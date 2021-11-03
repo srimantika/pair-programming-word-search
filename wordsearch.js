3d5f262754b97519cf0b8af81ef023c5fb2bc95a
srimantika <srimantika@gmail.com>
2021-11-03 21:35:45 +0000

    Wordsearch scenarios passed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,12 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    result = false; 
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)){
+            result = true;
+        } 
     }
-}
+    return result;
+}   
 
 module.exports = wordSearch
